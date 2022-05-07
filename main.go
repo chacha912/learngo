25709af74caccceb31a1f9754c9388228c3dfb24
chacha912 <dkwkdnwk912@gmail.com>
2022-05-08 02:22:41 +0900

    Add ChangeOwner method

--- main.go
+++ main.go
@@ -9,10 +9,11 @@ import (
 func main() {
 	account := accounts.NewAccount("yourim")
 	account.Deposit(10)
-	fmt.Println(account.Balance())
 	err := account.Withdraw(20)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(account.Balance())
+
+	account.ChangeOwner("ong")
+	fmt.Println(account.Balance(), account.Owner())
 }
