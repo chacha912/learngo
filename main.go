2c2b836e0fcb7e4fda7258bf89106e96cd72d90c
chacha912 <dkwkdnwk912@gmail.com>
2022-05-08 01:31:57 +0900

    feat: Init bank project

--- main.go
+++ main.go
@@ -1,18 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type person struct {
-	name    string
-	age     int
-	favFood []string
-}
+	"github.com/chacha912/learngo/accounts"
+)
 
 func main() {
-	favFood := []string{"kimchi"}
-	urim := person{"chayourim", 29, favFood}
-	fmt.Println(urim.name)
-
-	nico := person{name: "nico", age: 18, favFood: favFood}
-	fmt.Println(nico.name)
+	account := accounts.NewAccount("yourim")
+	fmt.Println(account)
+	fmt.Printf("%p", account)
 }
