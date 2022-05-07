a862c76d37595decbafcf404684e5e26eacb7ce9
USER <user@AL02001765.local>
2022-05-07 22:19:29 +0900

    functions - return two values

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func multiply(a, b int) int {
 	return a * b
 }
 
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
 func main() {
+
+	totalLength, upperName := lenAndUpper("urim")
 	fmt.Println(multiply(2, 2))
+	fmt.Println(totalLength, upperName)
 }
