df7138495d7b335fdbed98b730e07fc5a93a1c99
chacha912 <dkwkdnwk912@gmail.com>
2022-05-08 17:23:39 +0900

    JobScrapper - Print job info

--- job-scrapper/main.go
+++ job-scrapper/main.go
@@ -9,6 +9,14 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salay    string
+	summary  string
+}
+
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
@@ -22,6 +30,24 @@ func main() {
 func getPage(page int) {
 	pageUrl := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Requesting", pageUrl)
+	res, err := http.Get(pageUrl)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".jobCard_mainContent")
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		titleElem := card.Find(".jobTitle>a")
+		id, _ := titleElem.Attr("data-jk")
+		title := titleElem.Text()
+		location := card.Find(".companyLocation").Text()
+
+		fmt.Println("Review", i, id, title, location)
+	})
 }
 
 func getPages() int {
