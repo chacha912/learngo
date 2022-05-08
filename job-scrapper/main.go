63602265394659c20fbafdce0cd4631a2e99d43d
chacha912 <dkwkdnwk912@gmail.com>
2022-05-08 17:34:08 +0900

    JobScrapper - Extract job info

--- job-scrapper/main.go
+++ job-scrapper/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -20,14 +21,19 @@ type extractedJob struct {
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
+	var jobs []extractedJob
 	totalPages := getPages()
 
 	for i := 0; i < totalPages; i++ {
-		getPage(i)
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
 	}
+
+	fmt.Println(jobs)
 }
 
-func getPage(page int) {
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
 	pageUrl := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Requesting", pageUrl)
 	res, err := http.Get(pageUrl)
@@ -41,13 +47,31 @@ func getPage(page int) {
 
 	searchCards := doc.Find(".jobCard_mainContent")
 	searchCards.Each(func(i int, card *goquery.Selection) {
-		titleElem := card.Find(".jobTitle>a")
-		id, _ := titleElem.Attr("data-jk")
-		title := titleElem.Text()
-		location := card.Find(".companyLocation").Text()
-
-		fmt.Println("Review", i, id, title, location)
+		job := extractJob(card)
+		jobs = append(jobs, job)
 	})
+
+	return jobs
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	titleElem := card.Find(".jobTitle>a")
+	id, _ := titleElem.Attr("data-jk")
+	title := cleanString(titleElem.Text())
+	location := cleanString(card.Find(".companyLocation").Text())
+	salary := cleanString(card.Find(".salary-snippet").Text())
+	summary := cleanString(card.Find(".job-snippet").Text())
+
+	return extractedJob{
+		id:       id,
+		title:    title,
+		location: location,
+		salay:    salary,
+		summary:  summary}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
 func getPages() int {
