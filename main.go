f4b3d71789daecc74d21d9e4ee48e671947030e0
chacha912 <dkwkdnwk912@gmail.com>
2022-05-08 20:27:35 +0900

    Echo - Install echo package and Start server

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/chacha912/learngo/scrapper"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	scrapper.Scrape("term")
+	// Echo instance
+	e := echo.New()
+
+	// Routes
+	e.GET("/", handleHome)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func handleHome(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
